59966d8528ef83239a71d2233f08933fb0d34104
fireconta <fireconta02@gmail.com>
2025-04-24 15:49:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const config = {
-  firebaseConfig: {
-    apiKey: "AIzaSyDHRWs_TrSD7e3FAEDl6cIqUP48MecP_MM",
-    authDomain: "lacasadepizzas.firebaseapp.com",
-    projectId: "lacasadepizzas",
-    storageBucket: "lacasadepizzas.firebasestorage.app",
-    messagingSenderId: "400653207191",
-    appId: "1:400653207191:web:fa926964b578290556cbd2",
-    measurementId: "G-7N0T44N03D"
-  },
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHRWs_TrSD7e3FAEDl6cIqUP48MecP_MM",
+  authDomain: "lacasadepizzas.firebaseapp.com",
+  projectId: "lacasadepizzas",
+  storageBucket: "lacasadepizzas.firebasestorage.app",
+  messagingSenderId: "400653207191",
+  appId: "1:400653207191:web:fa926964b578290556cbd2",
+  measurementId: "G-7N0T44N03D"
+};
   numeroWhatsApp: "5511999999999",
   senhaAdmin: "admin",
   logoUrl: "https://imgur.com/a/ANag4EQ.jpeg"
