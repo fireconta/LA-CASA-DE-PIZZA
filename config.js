086134c2005b506beed9dff70986561b1ce24c19
fireconta <fireconta02@gmail.com>
2025-04-24 16:01:48 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDHRWs_TrSD7e3FAEDl6cIqUP48MecP_MM",
-  authDomain: "lacasadepizzas.firebaseapp.com",
-  projectId: "lacasadepizzas",
-  storageBucket: "lacasadepizzas.firebasestorage.app",
-  messagingSenderId: "400653207191",
-  appId: "1:400653207191:web:fa926964b578290556cbd2",
-  measurementId: "G-7N0T44N03D"
-};
+const config = {
+  firebaseConfig: {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+  },
   numeroWhatsApp: "5511999999999",
-  senhaAdmin: "admin",
-  logoUrl: "https://imgur.com/a/ANag4EQ.jpeg"
+  senhaAdmin: "admin123",
+  logoUrl: "https://i.imgur.com/ANag4EQ.png"
 };
