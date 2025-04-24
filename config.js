5d4252d45aecaa6474b1fb73985535cc46b07910
fireconta <fireconta02@gmail.com>
2025-04-24 16:43:08 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
   firebaseConfig: {
-  apiKey: "AIzaSyDHRWs_TrSD7e3FAEDl6cIqUP48MecP_MM",
-  authDomain: "lacasadepizzas.firebaseapp.com",
-  projectId: "lacasadepizzas",
-  storageBucket: "lacasadepizzas.firebasestorage.app",
-  messagingSenderId: "400653207191",
-  appId: "1:400653207191:web:968ca0613b699b7056cbd2",
-  measurementId: "G-8L13Y1PZ7C"
-},
+    apiKey: "AIzaSyDHRWs_TrSD7e3FAEDl6cIqUP48MecP_MM",
+    authDomain: "lacasadepizzas.firebaseapp.com",
+    projectId: "lacasadepizzas",
+    storageBucket: "lacasadepizzas.firebasestorage.app",
+    messagingSenderId: "400653207191",
+    appId: "1:400653207191:web:968ca0613b699b7056cbd2",
+    measurementId: "G-8L13Y1PZ7C"
+  },
   numeroWhatsApp: "5511999999999",
   senhaAdmin: "admin123",
-  logoUrl: "https://imgur.com/a/ANag4EQ"
+  logoUrl: "https://i.imgur.com/ANag4EQ.png"
 };
