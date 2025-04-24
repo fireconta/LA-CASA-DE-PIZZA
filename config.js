8c9c02ceadfb577862611b0f5ba5777d9cdd8ac7
fireconta <fireconta02@gmail.com>
2025-04-24 15:37:36 -0300

    Atualizando site

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  firebaseConfig: {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+  },
+  numeroWhatsApp: "5511999999999",
+  senhaAdmin: "admin123",
+  logoUrl: "https://i.imgur.com/ANag4EQ.png"
+};
