d50496de8b2ad21666d3f4f4b1bb0e113e42a75e
fireconta <fireconta02@gmail.com>
2025-04-26 23:13:42 -0300

    Delete cartoes.js

--- cartoes.js
+++
@@ -1,9 +0,0 @@
-const cartoes = JSON.parse(localStorage.getItem('cartoes')) || [];
-
-function salvarCartoes() {
-  localStorage.setItem('cartoes', JSON.stringify(cartoes));
-}
-
-if (!localStorage.getItem('cartoes')) {
-  salvarCartoes();
-}
