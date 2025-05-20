044a4411940ac25d17ce52d9fca535eb95afd78e
fireconta <fireconta02@gmail.com>
2025-05-20 15:16:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,19 +3,19 @@ const config = {
     instagram: 'https://instagram.com/lacasadepizzas2025',
     logo: 'https://imgur.com/3wbh5W3.jpeg',
     images: {
-        comboCasal: '',
-        comboFamilia: '',
-        comboSupremo: '',
-        pizzaCalabresa: '',
-        pizzaMargherita: '',
-        pizzaQuatroQueijos: '',
-        pizzaFrangoCatupiry: '',
-        pizzaPortuguesa: '',
-        pizzaPepperoni: '',
-        pizzaChocolate: '',
-        pizzaBrigadeiro: '',
-        pizzaBanana: '',
-        pizzaMorango: ''
+        comboCasal: 'https://imgur.com/3wbh5W3.jpeg',
+        comboFamilia: 'https://imgur.com/3wbh5W3.jpeg',
+        comboSupremo: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaCalabresa: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaMargherita: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaQuatroQueijos: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaFrangoCatupiry: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaPortuguesa: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaPepperoni: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaChocolate: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaBrigadeiro: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaBanana: 'https://imgur.com/3wbh5W3.jpeg',
+        pizzaMorango: 'https://imgur.com/3wbh5W3.jpeg'
     }
 };
 
