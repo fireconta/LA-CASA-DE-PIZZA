9230090360646208781ed2f52f338ced5cf0074b
fireconta <fireconta02@gmail.com>
2025-05-20 15:13:45 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 const config = {
-    logo: 'https://via.placeholder.com/120x120?text=La+Casa+de+Pizza\'s',
-    whatsapp: '55(XX)XXXXX-XXXX',
-    instagram: 'https://instagram.com/lacasadepizzas',
+    whatsapp: '+5562995771104',
+    instagram: 'https://instagram.com/lacasadepizzas2025',
+    logo: 'https://imgur.com/3wbh5W3.jpeg',
     images: {
-        comboCasal: 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092?q=80&w=100',
-        comboFamilia: 'https://images.unsplash.com/photo-1565299624946-b28fddf15867?q=80&w=100',
-        comboSupremo: 'https://images.unsplash.com/photo-1571066811602-716837d9f794?q=80&w=100',
-        pizzaCalabresa: 'https://images.unsplash.com/photo-1628840042765-3560de6d3d0e?q=80&w=100',
-        pizzaMargherita: 'https://images.unsplash.com/photo-1595854341625-fc2d94f075f5?q=80&w=100',
-        pizzaQuatroQueijos: 'https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?q=80&w=100',
-        pizzaFrangoCatupiry: 'https://images.unsplash.com/photo-1593560708920-61dd98c8bcef?q=80&w=100',
-        pizzaPortuguesa: 'https://images.unsplash.com/photo-1594007654729-407766e0e9c0?q=80&w=100',
-        pizzaPepperoni: 'https://images.unsplash.com/photo-1620371350502-999e9a7d80a4?q=80&w=100',
-        pizzaChocolate: 'https://images.unsplash.com/photo-1605472029625-08a449a9f387?q=80&w=100',
-        pizzaBrigadeiro: 'https://images.unsplash.com/photo-1596727147705-61d532a659bd?q=80&w=100',
-        pizzaBanana: 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092?q=80&w=100',
-        pizzaMorango: 'https://images.unsplash.com/photo-1605472029625-08a449a9f387?q=80&w=100'
+        comboCasal: '',
+        comboFamilia: '',
+        comboSupremo: '',
+        pizzaCalabresa: '',
+        pizzaMargherita: '',
+        pizzaQuatroQueijos: '',
+        pizzaFrangoCatupiry: '',
+        pizzaPortuguesa: '',
+        pizzaPepperoni: '',
+        pizzaChocolate: '',
+        pizzaBrigadeiro: '',
+        pizzaBanana: '',
+        pizzaMorango: ''
     }
 };
 
-window.config = config;
+export default config;
